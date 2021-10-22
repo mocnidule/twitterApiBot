0d3db77b0840982a3465a38996984b5b5d08ad64
Dusan Kovacevic <58424871+mocnidule@users.noreply.github.com>
2021-10-22 18:31:03 +0200

    Update keys.py

--- keys.py
+++ keys.py
@@ -1,7 +1,5 @@
-api_key = 'Sj4kRUmJE8wjzTDLc2asYpsLt'
-api_secret = 'y7tayZn2a7mlqBzttveS5WPk2rAlTXkBWM1ByFhUyHxX3Hh9bn'
-access_key = '1447141860829507586-ZkloUn4JUPdIaPtf4lYsx6eu2MbVKK'
-access_secret = '7MboPqEzFgj3MFc2oaaJX0Tqp8abeh17EMNz2ZUv3DFt4'
-bearer_token = 'AAAAAAAAAAAAAAAAAAAAAKX4UwEAAAAAIdHWW02zLzDpSxY36obGo3P%2FZtw' \
-               '%3DV7d58KRzcesFyp2ALfG1YBU1LQl8TzhauFY842UR4ooMOPfapP '
-
+api_key = ''
+api_secret = ''
+access_key = ''
+access_secret = ''
+bearer_token = ''
